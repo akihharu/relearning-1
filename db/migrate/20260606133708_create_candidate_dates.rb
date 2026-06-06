f0de37f7c51fcb854381f502aed1dcf4af470a38
akiharu <kobayashihannter@gmail.com>
2026-06-06 22:41:46 +0900

    候補日テーブルの作成と記述

---
+++ db/migrate/20260606133708_create_candidate_dates.rb
@@ -0,0 +1,10 @@
+class CreateCandidateDates < ActiveRecord::Migration[7.1]
+  def change
+    create_table :candidate_dates do |t|
+      t.references :event, null: false, foreign_key: true
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
