52c195ebdf49fe4b306581f4d04a31fa2b41ac64
akiharu <kobayashihannter@gail.com>
2026-06-13 23:32:25 +0900

    イベントモデルの記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  has_many :candidate_dates, dependent: :destroy
+  has_many :participants, dependent: :destroy
+
+  validates :title, presence: true, length: {maximum: 15}
+  validates :desc, length: {maximum: 100}
 end
