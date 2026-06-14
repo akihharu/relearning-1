877d657f61c3ceb9b9e18b687c77a5d325905d74
akiharu <kobayashihannter@gail.com>
2026-06-14 18:02:10 +0900

    参加者モデルの記述

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,7 @@
 class Participant < ApplicationRecord
   belongs_to :event
+  has_many :attendances, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 15}
+  validates :memo, length: {maximum: 500}
 end
